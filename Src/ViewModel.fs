9c0983e1231e888b71ecdc9d1a564d4dc386bcd7
Goswin <gwinsky@yahoo.com>
2021-04-24 10:53:40 +0200

    improve float convertor

--- Src/ViewModel.fs
+++ Src/ViewModel.fs
@@ -15,7 +15,94 @@ open Microsoft.FSharp
 open Microsoft.FSharp.Quotations
 open Microsoft.FSharp.Quotations.Patterns
 
-module ViewModel = 
+module ViewModel =        
+    
+
+    module NumberFormating = 
+        
+        // implementations copied from FsEx.MathUtil and FsEx.NiceString
+
+        /// CultureInfo.InvariantCulture
+        let invC = Globalization.CultureInfo.InvariantCulture
+        
+        /// American Englisch culture (used for float parsing)
+        let enUs = CultureInfo.GetCultureInfo("en-us")
+        
+        // German culture (used for float parsing)
+        //let deAt = CultureInfo.GetCultureInfo("de-at")
+
+        /// set this to change the printing of floats larger than 10'000
+        let mutable thousandSeparator = '\'' // = just one quote '
+
+        /// if the absolut value of a float is below this, display just Zero
+        /// default = Double.Epsilon = no rounding down
+        let mutable roundToZeroBelow = Double.Epsilon
+
+        let addThousandSeparators (s:string) =
+            let last = s.Length - 1         
+            let sb = Text.StringBuilder()
+            let inline add (c:char) = sb.Append(c) |> ignore
+            for i = 0 to last do
+                if i = 0 || i = last then 
+                    add s.[i]
+                elif i = 1 && s.[0] = '-' then 
+                    add s.[i]
+                else
+                    if (last - i + 1) % 3 = 0 then 
+                        add thousandSeparator
+                        add s.[i]
+                    else                
+                        add s.[i]
+            sb.ToString() 
+    
+        let int (x:int) = 
+            if abs(x) > 1000 then x.ToString() |> addThousandSeparators
+            else                  x.ToString() 
+
+        /// Formating with automatic precision 
+        /// e.g.: 0 digits behind comma if above 1000 
+        /// if there are more than 15 zeros behind the comma just '~0.0' will be displayed
+        /// if the value is smaller than NumberFormating.roundToZeroBelow '0.0' will be shown.
+        /// this is Double.Epsilon by default
+        let float  (x:float) =
+            if   Double.IsNaN x then "NaN"
+            elif x = Double.NegativeInfinity then "Negative Infinity"
+            elif x = Double.PositiveInfinity then "Positive Infinity"
+            elif x = -1.23432101234321e+308 then "-1.23432e+308 (=RhinoMath.UnsetValue)" // for https://developer.rhino3d.com/api/RhinoCommon/html/F_Rhino_RhinoMath_UnsetValue.htm
+            elif x = 0.0 then "0.0" // not "0" as in sprintf "%g"
+            else
+                let  a = abs x
+                if   a < roundToZeroBelow then "0.0"
+                elif a > 10000. then x.ToString("#")|> addThousandSeparators 
+                elif a > 1000.  then x.ToString("#")
+                elif a > 100.   then x.ToString("#.#" , invC)
+                elif a > 10.    then x.ToString("#.##" , invC)
+                elif a > 1.     then x.ToString("#.###" , invC)
+                elif a > 0.1    then x.ToString("0.####" , invC)
+                elif a > 0.01   then x.ToString("0.#####" , invC)
+                elif a > 0.001  then x.ToString("0.######" , invC)
+                elif a > 0.0001 then x.ToString("0.#######" , invC)
+                elif a > 0.000000000000001 then x.ToString("0.###############" , invC)// 15 decimal paces for doubles
+                else "~0.0"
+
+        /// A very tolerant custom float parser
+        /// ignores all non numeric characters ( expect leading '-' )
+        /// and considers '.' and  ',' as decimal point
+        /// does not allow for scientific notation
+        let tryParseFloatTolerant(s:string) =
+            let sb = Text.StringBuilder(s.Length)
+            for c in s do
+                if c >= '0' && c <= '9' then sb.Append(c) |> ignore
+                elif c = '.' then sb.Append(c) |> ignore
+                elif c = '-' && sb.Length = 0  then sb.Append(c) |> ignore //only add minus at start
+                elif c = ',' then sb.Append('.') |> ignore // german formating
+            match Double.TryParse(sb.ToString(), NumberStyles.Float, enUs) with
+            | true, f -> Some f
+            | _ ->   None 
+
+
+
+
 
     type ViewModelBase() = 
     
@@ -70,8 +157,9 @@ module ViewModel =
                         if targetType = typeof<string> then 
                             match value with 
                             | :? float as v ->  
-                                if snapToInt then v.AsString0    :> obj
-                                else              v.ToNiceString :> obj
+                                if snapToInt then v.ToString("0")|> NumberFormating.addThousandSeparators        
+                                else  NumberFormating.float v                                       
+                                :> obj
                             | _ -> value
                         else 
                             value
@@ -80,18 +168,13 @@ module ViewModel =
                         //match value with 
                         //| :? string -> printfn "convert BACK string to %s:%A" targetType.Name value
                         //| :? float -> printfn "convert BACK float to %s:%A" targetType.Name value
-                        //| _ -> printfn "convert BACK Other to %s:%A" targetType.Name value 
-                    
+                        //| _ -> printfn "convert BACK Other to %s:%A" targetType.Name value                     
                         if targetType = typeof<Double> then 
                             match value with 
                             | :? string as str ->  
-                                match UtilMath.tryParseFloatEnDe str with
-                                |Some v ->  
-                                    printfn "parse float  from'%s' = %A" str v
-                                    v :> obj
-                                |None ->  
-                                    printfn "parse float failed on '%s'" str
-                                    null
+                                match NumberFormating.tryParseFloatTolerant str with                                 
+                                |Some v -> v :> obj
+                                |None   -> null
                             | _ -> value
                         else 
                             value
